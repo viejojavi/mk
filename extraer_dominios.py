d514dc14170e1f00e0f921628eed768731c88754
viejojavi <56147006+viejojavi@users.noreply.github.com>
2024-06-25 16:19:48 -0500

    Update extraer_dominios.py

--- extraer_dominios.py
+++ extraer_dominios.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+import shutil
 
 # Función para limpiar el prefijo http:// y https:// de una URL y devolver solo el dominio
 def limpiar_url(url):
@@ -8,30 +9,34 @@ def limpiar_url(url):
     if dominio.startswith("www."):
         dominio = dominio[4:]  # Remover el prefijo 'www.'
     return dominio
+
 # Leer el listado de URLs desde un archivo
 with open('listado_urls.txt', 'r') as file:
     urls = file.readlines()
+
 # Verificar si las URLs fueron leídas correctamente
 print(f"URLs leídas: {len(urls)}")
 if len(urls) > 0:
     print(f"Primera URL: {urls[0].strip()}")
     print(f"Última URL: {urls[-1].strip()}")
+
 # Generar archivo con líneas de código antes y después de cada URL
 codigo_antes_con_codigos = "/ip/firewall/address-list/add list=bloqueo_mintic address="
 codigo_despues_con_codigos = " comment=Bloqueo_Mintic_by_Oscar_Castillo"
 codigo_con_delay = "delay 1"
-with open('urls_con_codigos.txt', 'w') as file:
+
+with open('address_list.txt', 'w') as file:
     for i, url in enumerate(urls):
         url = url.strip()
         if url:  # Asegurarse de que la URL no esté vacía
             dominio_limpio = limpiar_url(url)
             file.write(f"{codigo_antes_con_codigos}{dominio_limpio}{codigo_despues_con_codigos}\n")
-            
-            # Agregar línea con delay cada 50 líneas
-            if (i + 1) % 50 == 0:
-                file.write(codigo_con_delay + "\n")
+        
+        # Agregar línea con delay cada 50 líneas
+        if (i + 1) % 50 == 0:
+            file.write(f"{codigo_con_delay}\n")
 
-print("Archivo con URLs y códigos se ha guardado en 'urls_con_codigos.txt'.")
+print("Archivo con URLs y códigos se ha guardado en 'address_list.txt'.")
 
 # Función para dividir una URL en dominio y path
 def dividir_url(url):
@@ -46,9 +51,11 @@ def dividir_url(url):
     except Exception as e:
         print(f"Error al procesar la URL: {url} - {e}")
         return None, None
+
 # Generar archivo con dominio y path de cada URL
 codigo_antes_divididas = "/ip/proxy/access/add action=redirect action-data=ticcol.com/internet-sano-1 "
 codigo_despues_divididas = " comment=bloqueo_mintic"
+
 with open('urls_divididas.txt', 'w') as file:
     for i, url in enumerate(urls):
         url = url.strip()
@@ -63,8 +70,9 @@ with open('urls_divididas.txt', 'w') as file:
             # Agregar línea con delay cada 50 líneas
             if (i + 1) % 50 == 0:
                 file.write(codigo_con_delay + "\n")
+
 print("Archivo con dominios y paths se ha guardado en 'urls_divididas.txt'.")
+
 # Guardar el archivo con el nuevo nombre
-import shutil
-shutil.copy('urls_con_codigos.txt', 'listado_urls.rsc')
-print("Archivo 'urls_con_codigos.txt' se ha copiado como 'listado_urls.rsc'.")
+shutil.copy('address_list.txt', 'listado_urls.rsc')
+print("Archivo 'address_list.txt' se ha copiado como 'listado_urls.rsc'.")
