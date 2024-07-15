3d27225faa5af3370238a7ff259a51c5a59e88fd
viejojavi <56147006+viejojavi@users.noreply.github.com>
2024-07-15 11:01:50 -0500

    Update extraer_dominios.py

--- extraer_dominios.py
+++ extraer_dominios.py
@@ -43,3 +43,49 @@ print(f"Archivo con URLs y códigos se ha guardado en '{listado_completo_path}'.
 
 # Filtrar dominios únicos y escribir en un nuevo archivo
 with open(address_list_path, 'w') as file:
+    with open(listado_completo_path, 'r') as infile:
+        for line in infile:
+            parts = line.split(' ')
+            if len(parts) > 5:
+                domain = parts[5]  # Extraer el dominio del comando
+                if domain not in dominios_unicos:
+                    dominios_unicos.add(domain)
+                    file.write(line)
+
+print(f"Archivo con dominios únicos se ha guardado en '{address_list_path}'.")
+
+# Función para dividir una URL en dominio y path
+def dividir_url(url):
+    if not url.startswith(('http://', 'https://')):
+        url = 'http://' + url  # Añadir http:// si no está presente
+    try:
+        parsed_url = urlparse(url)
+        dominio = parsed_url.netloc
+        path = parsed_url.path
+        print(f"dst-host={dominio} path={path} para URL: {url}")
+        return dominio, path
+    except Exception as e:
+        print(f"Error al procesar la URL: {url} - {e}")
+        return None, None
+
+# Generar archivo con dominio y path de cada URL
+codigo_antes_divididas = "/ip/proxy/access/add action=redirect action-data=ticcol.com/internet-sano-1 "
+codigo_despues_divididas = " comment=bloqueo_mintic"
+
+access_path = 'access.rsc'
+with open(access_path, 'w') as file:
+    for i, url in enumerate(urls):
+        url = url.strip()
+        if url:  # Asegurarse de que la URL no esté vacía
+            dominio, path = dividir_url(url)
+            if dominio is not None:
+                if path and path != "/":
+                    file.write(f"{codigo_antes_divididas}dst-host={dominio} path={path}{codigo_despues_divididas}\n")
+                else:
+                    file.write(f"{codigo_antes_divididas}dst-host={dominio}{codigo_despues_divididas}\n")
+            
+            # Agregar línea con delay cada 50 líneas
+            if (i + 1) % 50 == 0:
+                file.write(codigo_con_delay + "\n")
+
+print(f"Archivo con dominios y paths se ha guardado en '{access_path}'.")
